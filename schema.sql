683b1c54d48e700fdd417238202134b8b8032084
Henrique <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-06-12 14:03:57 -0300

    feat: add schema.sql

---
+++ schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE departamento (
+	id_departamento INTEGER AUTO_INCREMENT,
+	nm_departamento VARCHAR(50) NOT NULL,
+	PRIMARY KEY(id_departamento)
+);
+
+CREATE TABLE funcionario (
+	id_funcionario INTEGER AUTO_INCREMENT,
+	nome VARCHAR(100) NOT NULL,
+	cpf CHAR(11) NOT NULL UNIQUE,
+	email VARCHAR(100) NOT NULL UNIQUE,
+	salario DECIMAL(10, 2),
+	is_gerente BOOLEAN NOT NULL,
+	pais VARCHAR(50) NOT NULL,
+	estado CHAR(2) NOT NULL,
+	cidade VARCHAR(100) NOT NULL,
+	bairro VARCHAR(100) NOT NULL,
+	logradouro VARCHAR(100) NOT NULL,
+	numero VARCHAR(10),
+	cep CHAR(9),
+	id_departamento INTEGER,
+	PRIMARY KEY(id_funcionario),
+	FOREIGN KEY (id_departamento) REFERENCES departamento(id_departamento) ON DELETE SET NULL
+);
+	
+	
