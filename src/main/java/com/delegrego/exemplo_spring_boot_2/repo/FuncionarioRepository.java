c09f6f99dc9d68956c4c3992a0e54fd501fecbf1
Henrique <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-06-14 22:25:02 -0300

    feat: adiciona segurança

--- src/main/java/com/delegrego/exemplo_spring_boot_2/repo/FuncionarioRepository.java
+++ src/main/java/com/delegrego/exemplo_spring_boot_2/repo/FuncionarioRepository.java
@@ -1,9 +1,11 @@
 package com.delegrego.exemplo_spring_boot_2.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.delegrego.exemplo_spring_boot_2.model.Funcionario;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer>{
-
+	Optional<Funcionario> findByEmail(String email);
 }
