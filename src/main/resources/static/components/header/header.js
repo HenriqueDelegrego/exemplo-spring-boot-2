58fc0f77eca54c8c9c3e73607670ec4ebe6b58a2
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-08-01 19:56:24 -0300

    feat: estiliza header

--- src/main/resources/static/components/header/header.js
+++ src/main/resources/static/components/header/header.js
@@ -1,8 +1,11 @@
-
 fetch('../../components/header/header.html')
     .then(response => response.text())
     .then(header => {
         document.getElementById('header').innerHTML = header;
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = '../../components/header/header-style.css';
+        document.head.appendChild(link);
     })
     .catch(error => {
         document.getElementById('header').innerHTML = `<p>Erro ao carregar o header</p>`;
