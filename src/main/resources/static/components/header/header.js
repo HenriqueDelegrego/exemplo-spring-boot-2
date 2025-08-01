9c8788a84e5face369ddc0d427b1dca80b6d2eba
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-08-01 16:27:41 -0300

    refactor: refactor header loading and minor HTML cleanup
    Simplified header.js by removing the loadHeader function and directly executing the fetch logic. Also removed unnecessary space in the hidden input tag in edicao-funcionario.html for consistency.

--- src/main/resources/static/components/header/header.js
+++ src/main/resources/static/components/header/header.js
@@ -1,20 +1,15 @@
 
-function loadHeader() {
-    fetch('../../components/header/header.html')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();
-        })
-        .then(data => {
-            document.getElementById('header').innerHTML = data;
-        })
-        .catch(error => {
-            console.error('Error loading header:', error);
-            document.getElementById('header').innerHTML = '<p>Error loading header.</p>';
-        });
-}
-
-// Run the function when the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', loadHeader);
+fetch('../../components/header/header.html')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
+    })
+    .catch(error => {
+        console.error('Error loading header:', error);
+        document.getElementById('header').innerHTML = '<p>Error loading header.</p>';
+    });
