72ac523b8cf92473639d6d083b4c70104cefb38b
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-08-15 10:34:06 -0300

    feat: adiciona derived query para encontrar um funcionário pelo cpf

--- src/main/java/com/delegrego/exemplo_spring_boot_2/repo/FuncionarioRepository.java
+++ src/main/java/com/delegrego/exemplo_spring_boot_2/repo/FuncionarioRepository.java
@@ -7,7 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.delegrego.exemplo_spring_boot_2.model.Funcionario;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
-	
+
 	// Derived query
 	Optional<Funcionario> findByEmail(String email);
+
+	Optional<Funcionario> findByCpf(String cpf);
 }
