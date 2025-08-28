e6d574ce427d06005741708c5b13f32520e6f872
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-08-28 17:24:40 -0300

    feat: cria inserções de departamentos e funcionários teste

--- docs/db/schema.sql
+++ docs/db/schema.sql
@@ -25,3 +25,9 @@ CREATE TABLE funcionario (
 	FOREIGN KEY (id_departamento) REFERENCES departamento(id_departamento),
 	FOREIGN KEY (criado_por) REFERENCES funcionario(id_funcionario)
 );
+
+INSERT INTO departamento (nm_departamento) VALUES ('Recursos Humanos');
+INSERT INTO departamento (nm_departamento) VALUES ('Tecnologia da Informação');
+
+INSERT INTO funcionario (nome, cpf, email, senha, data_nascimento, salario, gerente, estado, cidade, bairro, logradouro, numero, cep, id_departamento, criado_por)
+VALUES ('Henrique', '095', 'henrique@email.com', 'senha', '2001-11-10', 1000, TRUE, 'SC', 'Blumenau', 'Viktor Konder', 'Rua São Paulo', '11147', '89012001', 1, NULL);
