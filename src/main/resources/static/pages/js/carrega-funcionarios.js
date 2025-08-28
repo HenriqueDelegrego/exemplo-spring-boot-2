e55276d32fb2a406618e40b24b7d4b36f656b765
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-08-28 19:38:35 -0300

    feat: adiciona campo criado por na lista de funcionários

--- src/main/resources/static/pages/js/carrega-funcionarios.js
+++ src/main/resources/static/pages/js/carrega-funcionarios.js
@@ -44,6 +44,7 @@ async function carregarFuncionarios() {
     <strong>Salário:</strong> <span>R$ ${func.salario?.toFixed(2) || "0.00"}</span>
     <strong>Função:</strong> <span>${func.gerente ? "Gerente" : "Funcionário"}</span>
     <strong>Departamento:</strong> <span>${func.departamento?.nmDepartamento || "N/A"}</span>
+    <strong>Criado por:</strong> <span>${func.criadoPor?.nome || "N/A"}</span>
     <strong>Endereço:</strong>
     <span>${func.endereco?.logradouro || ""}, ${func.endereco?.numero || ""} - ${func.endereco?.bairro || ""}</span>
     <span>${func.endereco?.cidade || ""} - ${func.endereco?.estado || ""}</span>
