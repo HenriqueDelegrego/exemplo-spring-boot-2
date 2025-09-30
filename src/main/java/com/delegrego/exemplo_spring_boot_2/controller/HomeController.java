1a054f34bd6cc62d7b08484c6cd9252625fa8554
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-09-30 14:27:47 -0300

    comment: adiciona comentarios

--- src/main/java/com/delegrego/exemplo_spring_boot_2/controller/HomeController.java
+++ src/main/java/com/delegrego/exemplo_spring_boot_2/controller/HomeController.java
@@ -9,9 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+	/**
+	 * Redireciona para a página de login se o usuário não estiver autenticado
+	 * Caso contrário, redireciona para a página de lista de funcionários
+	 * @param autenticacao - Objeto de autenticação do Spring Security
+	 * @return Redirecionamento para a página apropriada
+	 */
 	@GetMapping
-	public String rootRedirect(Authentication authentication) {
-		if (authentication != null && authentication.isAuthenticated()) {
+	public String redirecionar(Authentication autenticacao) {
+		if (autenticacao != null && autenticacao.isAuthenticated()) {
 			return "redirect:/pages/html/lista-funcionarios.html";
 		}
 		return "redirect:/pages/html/login.html";
