75a0318af9b8ad131a8209c48a818fb488b1a0fd
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-10-22 15:05:10 -0300

    javadoc: adiciona javadoc na classe e métodos

--- src/main/java/com/delegrego/exemplo_spring_boot_2/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/delegrego/exemplo_spring_boot_2/exceptions/GlobalExceptionHandler.java
@@ -5,14 +5,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+/**
+ * Classe para tratamento global de exceções na aplicação.
+ */
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+	/**
+	 * Tratamento para RuntimeException.
+	 * @param ex - exceção lançada
+	 * @return ResponseEntity com status 400 e mensagem da exceção
+	 */
 	@ExceptionHandler(RuntimeException.class)
 	public ResponseEntity<String> tratarRuntimeException(RuntimeException ex) {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
 	}
 
+	/**
+	 * Tratamento para EmailDuplicadoException.
+	 * @param ex - exceção lançada
+	 * @return ResponseEntity com status 400 e mensagem da exceção
+	 */
 	@ExceptionHandler(EmailDuplicadoException.class)
 	public ResponseEntity<String> tratarEmailDuplicadoException(EmailDuplicadoException ex) {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
