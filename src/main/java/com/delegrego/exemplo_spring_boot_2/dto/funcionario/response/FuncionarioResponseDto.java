cded278716d37203896a3bd9793f238521fb7fd1
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-10-24 20:46:07 -0300

    remove anotações de validação de dto de response

--- src/main/java/com/delegrego/exemplo_spring_boot_2/dto/funcionario/response/FuncionarioResponseDto.java
+++ src/main/java/com/delegrego/exemplo_spring_boot_2/dto/funcionario/response/FuncionarioResponseDto.java
@@ -6,14 +6,6 @@ import java.time.LocalDate;
 import com.delegrego.exemplo_spring_boot_2.dto.departamento.response.DepartamentoDto;
 import com.delegrego.exemplo_spring_boot_2.dto.endereco.EnderecoDto;
 
-import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.PositiveOrZero;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,33 +21,20 @@ public class FuncionarioResponseDto {
 
 	private int idFuncionario;
 
-	@NotBlank
-	@Size(max = 100)
 	private String nome;
 
-	@NotBlank
-	@Pattern(regexp = "\\d{11}")
 	private String cpf;
 
-	@NotBlank
-	@Email
-	@Size(max = 100)
 	private String email;
 
-	@NotNull
-	@PastOrPresent
 	private LocalDate dataNascimento;
 
-	@Digits(integer = 10, fraction = 2)
-	@PositiveOrZero
 	private BigDecimal salario;
 
 	private boolean gerente;
 
-	@NotNull
 	private EnderecoDto endereco;
 
-	@NotNull
 	private DepartamentoDto departamento;
 
 	private String criadoPor;
