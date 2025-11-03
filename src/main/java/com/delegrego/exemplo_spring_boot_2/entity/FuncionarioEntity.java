4b45f158555986bdb2e791443f94208dd19cdfa3
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-11-03 19:14:25 -0300

    feat(nullable-column): remove anotação redundante

--- src/main/java/com/delegrego/exemplo_spring_boot_2/entity/FuncionarioEntity.java
+++ src/main/java/com/delegrego/exemplo_spring_boot_2/entity/FuncionarioEntity.java
@@ -73,7 +73,7 @@ public class FuncionarioEntity {
 	private DepartamentoEntity departamento;
 
 	@ManyToOne
-	@JoinColumn(name = "criado_por", nullable = true)
+	@JoinColumn(name = "criado_por")
 	private FuncionarioEntity criadoPor;
 
 }
