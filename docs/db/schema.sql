e15d8abe1d367e06ac8e5bf0b14ed04d94b2080c
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-12-04 20:35:12 -0300

    refactor(sql-schema): separate ddl and dml scripts into two different files

--- docs/db/schema.sql
+++ docs/db/schema.sql
@@ -25,12 +25,3 @@ CREATE TABLE funcionario (
 	FOREIGN KEY (id_departamento) REFERENCES departamento(id_departamento),
 	FOREIGN KEY (criado_por) REFERENCES funcionario(id_funcionario)
 );
-
-INSERT INTO departamento (nm_departamento) VALUES ('Recursos Humanos');
-INSERT INTO departamento (nm_departamento) VALUES ('Tecnologia da Informação');
-
-INSERT INTO funcionario (nome, cpf, email, senha, data_nascimento, salario, gerente, estado, cidade, bairro, logradouro, numero, cep, id_departamento, criado_por)
-VALUES ('Henrique', '11111111111', 'henrique@email.com', 'senha', '2001-11-10', 1000, TRUE, 'SC', 'Blumenau', 'Viktor Konder', 'Rua São Paulo', '11147', '89012001', 1, NULL);
-
-INSERT INTO funcionario (nome, cpf, email, senha, data_nascimento, salario, gerente, estado, cidade, bairro, logradouro, numero, cep, id_departamento, criado_por)
-VALUES ('Ana', '22222222222', 'ana@email.com', 'senha', '1995-05-20', 2000, FALSE, 'SC', 'Florianópolis', 'Centro', 'Rua das Flores', '2020', '88015000', 2, 1);
